a3626a53b5dcaae1004b1acfb1bae393479ac1e6
Zwd118 <1870208989@qq.com>
2022-07-31 22:26:19 +0800

    2022.7.31

---
+++ Zwd1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Zwd1 {
+  public static void main (String [ ] args) {
+    int a = 10;
+	int b = 3;
+	int c=687;
+	Scanner sc = new Scanner(System.in);
+	 System.out.println("请您输入：");
+	int d = sc.nextInt();
+		 System.out.println("你输入的是："+d);
+	//System.out.println(b);//2
+  }
+}
